5609f5ad291f60804d86b7766ecd8d3fb1b1ebbf
NFPerera <niko22602@gmail.com>
2022-11-21 15:48:51 -0300

    fix RPC particle size

--- ProjectBeta/Assets/_ProjectBeta/Scripts/ParticleController.cs
+++ ProjectBeta/Assets/_ProjectBeta/Scripts/ParticleController.cs
@@ -1,10 +1,11 @@
 using System;
 using _ProjectBeta.Scripts.PlayerScrips;
+using Photon.Pun;
 using UnityEngine;
 
 namespace _ProjectBeta.Scripts
 {
-    public class ParticleController : MonoBehaviour
+    public class ParticleController : MonoBehaviourPun
     {
         private bool _followsPlayer;
         private float _time;
@@ -15,6 +16,14 @@ namespace _ProjectBeta.Scripts
             _modelTransform = modelTransform;
             _time = lifeTime;
 
+            photonView.RPC(nameof(RPC_SetSizeParticle), RpcTarget.All, size);
+            
+            _followsPlayer = followsPlayer;
+        }
+     
+        [PunRPC]
+        private void RPC_SetSizeParticle(Vector3 size)
+        {
             for (int i = 0; i < transform.childCount; i++)
             {
                 var child = transform.GetChild(i);
@@ -22,12 +31,11 @@ namespace _ProjectBeta.Scripts
                 child.transform.localScale = size;
             }
             transform.localScale = size;
-            _followsPlayer = followsPlayer;
         }
-     
 
         private void Update()
         {
+            if(!photonView.IsMine) return;
             _time -= Time.deltaTime;
 
             if (_followsPlayer)
@@ -37,7 +45,7 @@ namespace _ProjectBeta.Scripts
             
             if (_time < 0)
             {
-                Destroy(gameObject);
+                PhotonNetwork.Destroy(gameObject);
             }
         }
     }
